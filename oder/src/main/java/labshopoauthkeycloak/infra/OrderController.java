d611967a899dbf221f0934e3219ab9f5d6a630e3
uengineYSW <112850933+uengineYSW@users.noreply.github.com>
2023-08-03 18:28:12 +0900

    Update OrderController.java

--- oder/src/main/java/labshopoauthkeycloak/infra/OrderController.java
+++ oder/src/main/java/labshopoauthkeycloak/infra/OrderController.java
@@ -1,20 +1,53 @@
 package labshopoauthkeycloak.infra;
 
-import java.util.Optional;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.transaction.Transactional;
-import labshopoauthkeycloak.domain.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+
 @RestController
-// @RequestMapping(value="/orders")
-@Transactional
+@RequestMapping("/orders")
 public class OrderController {
 
-    @Autowired
-    OrderRepository orderRepository;
+    @GetMapping("/placeOrder")
+    @RolesAllowed({"ROLE_CUSTOMER"})
+    public String placeAnOrder() {
+        String preferred_username = getPreferred_username();        
+        return "<center> Hi~ " + preferred_username + 
+                        ". Welcome to 12stMall.<br/><br/> We've Good Products, You can place an Order.";
+    }
+
+    @GetMapping("/manageOrder")
+    @RolesAllowed({"ROLE_ADMIN"})
+    public String orderManage() {
+        String preferred_username = getPreferred_username();        
+        return "<center> Hi~ " + preferred_username + 
+                        ". Welcome to 12stMall.<br/><br/> You can manage Order Admin Jobs.";
+    }
+
+    @GetMapping("/getUser")
+    @PermitAll
+    public String getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof Jwt) {
+            Jwt jwt = (Jwt) authentication.getPrincipal();
+            return String.format("You are [%s] with e-mail address [%s].%n",
+                jwt.getSubject(), jwt.getClaimAsString("email"));
+        }
+        else {
+            return "Something went wrong; authentication is not provided by IAP/JWT.\n";
+        }
+    }
+
+    private String getPreferred_username() {
+        Jwt jwt = (Jwt)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return jwt.getClaimAsString("preferred_username");
+         
+    }
 }
